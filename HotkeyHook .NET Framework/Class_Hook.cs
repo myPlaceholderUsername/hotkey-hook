352f98a37a9da0b7e42144c1f98abf663cbcce43
whereAmI0 <eric0work0email@gmail.com>
2024-04-14 23:19:53 +0700

    Added code to prevent hook from being triggered multiple times when hotkey is held down

--- HotkeyHook .NET Framework/Class_Hook.cs
+++ HotkeyHook .NET Framework/Class_Hook.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HotkeyHook
 {
@@ -11,23 +12,32 @@ namespace HotkeyHook
     {
         public delegate void HookedFunction(List<object> inArgs);
 
-        static List<Class_Hook> _myHooks = new List<Class_Hook>();
-        static Dictionary<string, HookedFunction> _dict_HotkeyAndFunction = new Dictionary<string, HookedFunction>();
+        internal static List<Class_Hook> MyHooks { get; private set; } = new List<Class_Hook>();
+        internal static Dictionary<string, HookedFunction> Dict_HotkeyAndFunction { get; set; } = new Dictionary<string, HookedFunction>();
 
         // Properties
-        IntPtr _hookId;
-        string _strHotkey = String.Empty;
-        int _hotkeyVkCode;
+        public IntPtr HookId { get; internal set; }
+        internal string StrHotkey { get; set; }
+        internal Key Hotkey { get; set; }
 
-        HookedFunction _functionToCallOnKeyPress;
-        List<object> _args = new List<object>();
+        private Class_HotkeyHookManager.HookProc HookProc { get; set; }     // Prevents disposal of callback that is still used
+        internal HookedFunction FunctionToCallOnKeyPress { get; set; }
+        public List<object> Args { get; set; }
 
-        internal Class_Hook(IntPtr inHookId, string inHotKey, HookedFunction inFunctionToCallOnKeyPress)
+        /// <summary>
+        /// This variable is used to prevent the hook from being triggered multiple times when hotkey is being held down
+        /// Hook will only trigger if hotkey is down AND this variable is true
+        /// This variable is true upon key up, and false upon key down
+        /// </summary>
+        internal bool isHotkeyReleased { get; set; } = false;
+
+        internal Class_Hook(IntPtr inHookId, string inHotKey, Class_HotkeyHookManager.HookProc inHookProc, HookedFunction inFunctionToCallOnKeyPress)
         {
             this.HookId = inHookId;
             this.StrHotkey = inHotKey;
-            this.HotkeyVkCode = dict_HotkeyAndVkCode[this.StrHotkey];
+            this.Hotkey = Dict_StrHotkeyAndKey[this.StrHotkey];
 
+            this.HookProc = inHookProc;
             this.FunctionToCallOnKeyPress = inFunctionToCallOnKeyPress;
 
             Dict_HotkeyAndFunction.Add(this.StrHotkey, inFunctionToCallOnKeyPress);
@@ -40,11 +50,11 @@ namespace HotkeyHook
             MyHooks.Remove(this);
         }
 
-        internal static bool isHotkeyExist(int inHotkeyVkCode)
+        internal static bool isHotkeyExist(Key inHotkeyVkCode)
         {
             for (int i = 0; i < MyHooks.Count; i++)
             {
-                if (MyHooks[i].HotkeyVkCode == inHotkeyVkCode)
+                if (MyHooks[i].Hotkey == inHotkeyVkCode)
                     return true;
             }
             return false;
@@ -52,34 +62,23 @@ namespace HotkeyHook
 
         internal static bool isHotkeySupported(string inHotkey)
         {
-            return dict_HotkeyAndVkCode.ContainsKey(inHotkey);
+            return Dict_StrHotkeyAndKey.ContainsKey(inHotkey);
         }
 
-        internal static Dictionary<string, int> dict_HotkeyAndVkCode { get; private set; } = new Dictionary<string, int>()
+        internal static Dictionary<string, Key> Dict_StrHotkeyAndKey { get; private set; } = new Dictionary<string, Key>()
         {
-            { "F1", 0x70 },
-            { "F2", 0x71 },
-            { "F3", 0x72 },
-            { "F4", 0x73 },
-            { "F5", 0x74 },
-            { "F6", 0x75 },
-            { "F7", 0x76 },
-            { "F8", 0x77 },
-            { "F9", 0x78 },
-            { "F10", 0x79 },
-            { "F11", 0x7A },
-            { "F12", 0x7B },
+            { "F1", Key.F1 },
+            { "F2", Key.F2 },
+            { "F3", Key.F3 },
+            { "F4", Key.F4 },
+            { "F5", Key.F5 },
+            { "F6", Key.F6 },
+            { "F7", Key.F7 },
+            { "F8", Key.F8 },
+            { "F9", Key.F9 },
+            { "F10", Key.F10 },
+            { "F11", Key.F11 },
+            { "F12", Key.F12 },
         };
-
-        //Get Set
-        public static List<Class_Hook> MyHooks { get => _myHooks; private set => _myHooks = value; }
-        internal static Dictionary<string, HookedFunction> Dict_HotkeyAndFunction { get => _dict_HotkeyAndFunction; set => _dict_HotkeyAndFunction = value; }
-
-        public IntPtr HookId { get => _hookId; internal set => _hookId = value; }
-        internal string StrHotkey { get => _strHotkey; set => _strHotkey = value; }
-        internal int HotkeyVkCode { get => _hotkeyVkCode; set => _hotkeyVkCode = value; }
-
-        internal HookedFunction FunctionToCallOnKeyPress { get => _functionToCallOnKeyPress; set => _functionToCallOnKeyPress = value; }
-        public List<object> Args { get => _args; set => _args = value; }
     }
 }
--- HotkeyHook .NET Framework/Class_HotkeyHookManager.cs
+++ HotkeyHook .NET Framework/Class_HotkeyHookManager.cs
@@ -2,14 +2,16 @@
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Input;
 
 namespace HotkeyHook
 {
     public class Class_HotkeyHookManager
     {
-        delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        internal delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
 
         const int WH_KEYBOARD = 2;        //Hook id for key stroke
         const int WH_KEYBOARD_LL = 13;
@@ -25,15 +27,16 @@ namespace HotkeyHook
 
             IntPtr moduleHandle = GetModuleHandle(currentModule.ModuleName);
 
-            IntPtr hookId = SetWindowsHookExA(WH_KEYBOARD,
-                                              HookCallback,
-                                              moduleHandle,
-                                              inThreadId);
-
             //IntPtr hookId = SetWindowsHookExA(WH_KEYBOARD,
             //                                  HookCallback,
-            //                                  IntPtr.Zero,
+            //                                  moduleHandle,
             //                                  inThreadId);
+
+            HookProc newHookProc = new HookProc(HookCallback);
+            IntPtr hookId = SetWindowsHookExW(WH_KEYBOARD_LL,
+                                              newHookProc,
+                                              IntPtr.Zero,
+                                              0);
 
             Console.WriteLine("hookId: " + hookId.ToString());
             if (hookId == IntPtr.Zero)
@@ -42,58 +45,61 @@ namespace HotkeyHook
                 return IntPtr.Zero;
             }
 
-            new Class_Hook(hookId, inHotKey, inFunctionToRunOnKeyUp);
+            new Class_Hook(hookId, inHotKey, newHookProc, inFunctionToRunOnKeyUp);
 
             Console.WriteLine("Hotkey \"" + inHotKey + "\" is successfully set");
             return hookId;
         }
 
-        public static void UnsetHotkey(IntPtr inHookId)
+        public static void UnsetHotkey(string inStrHotkey)
         {
-            Class_Hook removedHook = Class_Hook.MyHooks.First(hook => hook.HookId == inHookId);
-            if (removedHook == null)
+            Class_Hook removedHook;
+            try
+            {
+                removedHook = Class_Hook.MyHooks.First(hook => hook.StrHotkey == inStrHotkey);
+            }
+            catch
             {
                 Console.WriteLine("Trying to remove hook that doesn't exist");
                 return;
             }
 
+            UnhookWindowsHookEx(removedHook.HookId);
             removedHook.DeleteHook();
-            UnhookWindowsHookEx(inHookId);
+        }
+
+        public static void UnsetAllHotkeys()
+        {
+            while (Class_Hook.MyHooks.Count > 0)
+            {
+                UnsetHotkey(Class_Hook.MyHooks[0].StrHotkey);
+            }
         }
 
         // wParam = The key that triggers the event
         // lParam = Key event
         static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode <= 0)
+            if (nCode < 0)
                 return IntPtr.Zero;
 
-            int vkCode = Marshal.ReadInt32(wParam); // Read vkCode (int) at wParam (IntPtr)
+            foreach (Class_Hook hook in Class_Hook.MyHooks)
+            {
+                if (Keyboard.IsKeyDown(hook.Hotkey) &&
+                    hook.isHotkeyReleased)
+                {
+                    hook.isHotkeyReleased = false;
 
-            if (!Class_Hook.isHotkeyExist(vkCode))
-                return IntPtr.Zero;
+                    hook.FunctionToCallOnKeyPress.Invoke(hook.Args);
 
-            Class_Hook connectedHook = Class_Hook.MyHooks.First(hook => hook.HotkeyVkCode == vkCode);
-            if (isKeyUp(lParam))
-                connectedHook.FunctionToCallOnKeyPress.Invoke(connectedHook.Args);
+                    return IntPtr.Zero;
+                }
 
-            //foreach (Class_Hook hook in Class_Hook.MyHooks)
-            //{
-            //    if (Keyboard.IsKeyUp(Key.))
-            //    {
-
-            //    }
-            //}
+                if (Keyboard.IsKeyUp(hook.Hotkey))
+                    hook.isHotkeyReleased = true;
+            }
 
             return IntPtr.Zero;
-        }
-
-        const int WM_KEYUP = 0x0101;
-        const int WM_SYSKEYUP = 0x0105;
-        static bool isKeyUp(IntPtr inMessage)
-        {
-            return (inMessage == (IntPtr)WM_KEYUP ||
-                    inMessage == (IntPtr)WM_SYSKEYUP);
         }
 
         // idHook = Type of message/hook
@@ -101,7 +107,9 @@ namespace HotkeyHook
         // hmod = Handle to module of this DLL
         // dwThreadId = Thread ID to the project that calls this DLL
         [DllImport("user32.dll")]
-        static extern IntPtr SetWindowsHookExA(int idHook, HookProc lpfn, IntPtr hmod, uint dwThreadId);
+        static extern IntPtr SetWindowsHookExW(int idHook, HookProc lpfn, IntPtr hmod, uint dwThreadId);
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
